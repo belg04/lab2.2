ec55eeb4c2f8260e5a3efe158c2345bfc39e2967
glebe <glebe@BELG04>
2022-10-12 10:18:38 +0200

    Добавьте файлы проекта.

---
+++ lab2.2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Лукьянов Глеб     22-исп-2\1      Уровень:Высокий   Номер:2
+Console.Write("Введите двухзначное число:");
+int numder = int.Parse(Console.ReadLine());
+Console.Write("Введите число n чтобы определть входит ли это число :");
+int n = int.Parse(Console.ReadLine());
+
+    if ((numder % 10 == 5) || (numder / 10 == 5))
+    {
+        Console.WriteLine("В числе есть 5");
+    }
+
+    else
+    {
+        Console.WriteLine("В числе нет 5");
+    }
+
+
+    if ((numder % 10 == n) || (numder / 10 == n))
+    {
+        Console.WriteLine("В числе есть n");
+    }
+
+    else
+    {
+        Console.WriteLine("В числе нет n");
+    }
+
+
+
+
+
